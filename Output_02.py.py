ac5a5e3f4783e76a121f3cc45ed274c736255d00
20KASHYAP25 <96770907+20KASHYAP25@users.noreply.github.com>
2022-01-25 01:57:12 +0530

    Assignment_02
    21104001

---
+++ Output_02.py.py
@@ -0,0 +1,30 @@
+Windows PowerShell
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+Install the latest PowerShell for new features and improvements! https://aka.ms/PSWindows
+
+PS C:\Users\kashy> & C:/Users/kashy/AppData/Local/Programs/Python/Python310/python.exe "d:/Code with/Assignment_02.py"
+35
+egaugnal evitisnes esac a si nohtyP
+a case sensitive 
+Python is object oreinted language
+10
+Pythonisacasesensitivelanguage
+Hey, Kashyap Raina Here! 
+My SID is 21104001
+I am from Electrical Engineering department and my CGPA is 9.0
+8
+58
+50
+224 40
+14 0
+Enter first number: 40
+Enter second number: 50
+Enter third number: 30
+The largest number is 50.0
+type: What is name of that person
+yes
+enter the length of first side:3
+enter the length of second side:4
+enter the length of third side:5
+yes
