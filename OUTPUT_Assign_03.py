13466c16f8956770959f7c61ab24f31ecb563927
20KASHYAP25 <96770907+20KASHYAP25@users.noreply.github.com>
2022-02-14 23:46:03 +0530

    Assignment_03

---
+++ OUTPUT_Assign_03.py
@@ -0,0 +1,51 @@
+PS D:\Code with> & C:/Users/kashy/AppData/Local/Microsoft/WindowsApps/python3.9.exe "d:/Code with/Assignment_03.py"
+ENTER ANY STRING: int
+{'i': 1, 'n': 1, 't': 1}
+
+
+Please Enter The Day: 14
+Please Enter The Month: 1
+Please Enter The Year: 2022
+Next Date Is:  15 / 1 / 2022
+Enter integers seperated by spaces:4 16 20
+[(4, 16), (16, 256), (20, 400)]
+ENTER YOUR GRADE BETWEEN 4 TO 10: 6
+Your Grade is 'C+' and Average performance
+
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+Enter SID: 21104001
+Enter Name: Kashyap Raina
+Do you want to enter another student details(Y or N): Y
+Enter SID: 21104094
+Enter Name: Vaibhav 
+Do you want to enter another student details(Y or N): Y
+Enter SID: 21104002    
+Enter Name: Naheed Anjum
+Do you want to enter another student details(Y or N): N
+a. {21104001: 'Kashyap Raina', 21104094: 'Vaibhav ', 21104002: 'Naheed Anjum'}
+b. {21104001: 'Kashyap Raina', 21104002: 'Naheed Anjum', 21104094: 'Vaibhav '}
+c. {21104001: 'Kashyap Raina', 21104002: 'Naheed Anjum', 21104094: 'Vaibhav '}
+Please Enter SID Of The Student You Want To Search: 21104002
+d. Student With The Given SID Is:  Naheed Anjum
+Enter The Number Of Terms In The Seires: 6
+Resultant Fibonacci sequence: 
+0
+1
+1
+2
+3
+5
+Average Of The Resultant Fibonacci Series =  2.0
+a.  {2, 3, 9, 10, 12, 14, 15, 20}
+b.  {1, 2, 3, 5, 10, 12, 13, 14, 15, 17, 20}
+c.  {9, 6}
+d.  {1, 2, 4, 5, 7, 8, 10}
+e.  {8, 4, 7}
+PS D:\Code with> 
+PS D:\Code with>
